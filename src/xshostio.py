6dc53ce26bd0444e0b09a7c799c06381dc1afdf3
Dave Vandenbout <xesscorp@xess.com>
2012-03-29 22:52:00 -0400

    - Reformatted GPL block.

--- src/xshostio.py
+++ src/xshostio.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
-# /***********************************************************************************
-# *   This program is free software; you can redistribute it and/or
-# *   modify it under the terms of the GNU General Public License
-# *   as published by the Free Software Foundation; either version 2
-# *   of the License, or (at your option) any later version.
-# *
-# *   This program is distributed in the hope that it will be useful,
-# *   but WITHOUT ANY WARRANTY; without even the implied warranty of
-# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# *   GNU General Public License for more details.
-# *
-# *   You should have received a copy of the GNU General Public License
-# *   along with this program; if not, write to the Free Software
-# *   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
-# *   02111-1307, USA.
-# *
-# *   (c)2012 - X Engineering Software Systems Corp. (www.xess.com)
-# ***********************************************************************************/
+# **********************************************************************
+#   This program is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU General Public License
+#   as published by the Free Software Foundation; either version 2
+#   of the License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+#   02111-1307, USA.
+#
+#   (c)2012 - X Engineering Software Systems Corp. (www.xess.com)
+# **********************************************************************
 
 """
 Base object for performing USB I/O between XESS board and host PC.
@@ -35,8 +34,8 @@ class XsHostIo:
 
     """Base object for performing USB I/O between XESS board and host PC."""
 
-    USER1_INSTR = XsBitarray("000010"[::-1])
-    USER2_INSTR = XsBitarray("000011"[::-1])
+    USER1_INSTR = XsBitarray('000010'[::-1])
+    USER2_INSTR = XsBitarray('000011'[::-1])
 
     def __init__(
         self,
@@ -47,7 +46,7 @@ class XsHostIo:
         """Setup the parameters for the USB I/O link between the PC and the XESS board."""
 
         self._xsusb_id = xsusb_id
-        if type(module_id)is int:
+        if type(module_id) is int:
             self._module_id = XsBitarray.from_int(module_id, 8)
         else:
             self._module_id = XsBitarray(module_id)
@@ -65,15 +64,14 @@ class XsHostIo:
         assert self.xsjtag != None
         self.xsjtag.reset_tap()  # Reset TAP FSM to test-logic-reset state.
         # Send TAP FSM to the shift-ir state.
-        self.xsjtag.go_thru_tap_states("Run-Test/Idle",
-                "Select-DR-Scan", "Select-IR-Scan", "Capture-IR",
-                "Shift-IR")
+        self.xsjtag.go_thru_tap_states('Run-Test/Idle', 'Select-DR-Scan'
+                , 'Select-IR-Scan', 'Capture-IR', 'Shift-IR')
         # Now enter the USER1 JTAG instruction into the IR and go to the update-ir state.
         self.xsjtag.shift_tdi(tdi=self.user_instr, do_exit_shift=True)
         # USER instruction is now active, so transfer to the shift-dr state where
         # data transfers will occur.
-        self.xsjtag.go_thru_tap_states("Update-IR",
-                "Select-DR-Scan", "Capture-DR", "Shift-DR")
+        self.xsjtag.go_thru_tap_states('Update-IR', 'Select-DR-Scan',
+                'Capture-DR', 'Shift-DR')
 
     def reset(self):
         """Reset the USB I/O link."""
@@ -83,8 +81,8 @@ class XsHostIo:
     def send_rcv(self, payload, num_result_bits):
         """Send a bit array payload and then return a results bit array with num_result_bits."""
 
-        logging.debug("Send " + str(payload.length()) + "bits. Receive "
-                       + str(num_result_bits) + " bits.")
+        logging.debug('Send ' + str(payload.length()) + 'bits. Receive '
+                       + str(num_result_bits) + ' bits.')
 
         # Create the TDI bit array by concatenating the module ID, number of bits in the payload, and the payload bits.
         tdi_bits = XsBitarray()
@@ -94,11 +92,11 @@ class XsHostIo:
         tdi_bits.extend(num_payload_bits)
         tdi_bits.extend(payload)
 
-        logging.debug("Module ID = " + repr(self._module_id))
-        logging.debug("# payload bits = " + repr(num_payload_bits))
-        logging.debug("payload = " + repr(payload))
-        logging.debug("# TDI bits = " + str(tdi_bits.length()))
-        logging.debug("TDI = " + repr(tdi_bits))
+        logging.debug('Module ID = ' + repr(self._module_id))
+        logging.debug('# payload bits = ' + repr(num_payload_bits))
+        logging.debug('payload = ' + repr(payload))
+        logging.debug('# TDI bits = ' + str(tdi_bits.length()))
+        logging.debug('TDI = ' + repr(tdi_bits))
 
         # Send the TDI bits.
         self.xsjtag.shift_tdi(tdi=tdi_bits)
