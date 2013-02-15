6b60bbfad9f01102cb79e1c2d7a106e56b6fc918
Dave Vandenbout <xesscorp@xess.com>
2013-02-15 15:43:11 -0500

    Updated files for version number 0.1.3

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup
 
 setup(
     name='XsTools',
-    version='0.1.2',
+    version='0.1.3',
     author='XESS Corp.',
     author_email='info@xess.com',
     packages=['xstools'],
     package_data={'xstools': ['xula*/*/*/*.bit']},
     scripts=['bin/xstest.py', 'bin/xsload.py', 'bin/xsusbprg.py'],
     url='http://pypi.python.org/pypi/XsTools/',
-    license='LICENSE.TXT',
     description='Classes for interfacing with XESS FPGA boards via USB.',
     long_description=open('README.txt').read(),
     install_requires=['pypubsub', 'pyusb', 'bitarray', 'intelhex'],
+    classifiers=['License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',]
     )
 
