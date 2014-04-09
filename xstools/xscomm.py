b17ac843044f1a0243af61a521b316d9d002ae78
Dave Vandenbout <xesscorp@xess.com>
2014-04-09 13:02:22 -0400

    Modified send/receive methods for comm channels.

--- xstools/xscomm.py
+++ xstools/xscomm.py
@@ -25,30 +25,33 @@ Class for managing communication streams to/from the FPGA.
 
 from xsmemio import *
 
+
 class XsCommException(Exception):
     pass
 
+
 class XsComm:
+
     """
     This class instantiates an object that will communicate bidirectionally
     through the JTAG port of the FPGA to a module within the FPGA.
     """
 
     # Constants for the communication interface.
-    _FIFO_ADDR = 0 # Address for sending/receiving data to/from the FPGA.
-    _STATUS_ADDR = 1 # Address for getting status on the send/recv comm channels.
-    _CONTROL_ADDR = 1 # Write to this address to reset the entire comm channel.
-    _DN_FREE_ADDR = 2 # Read this address to get the amount of free space in the download FIFO.
-    _UP_USED_ADDR = 3 # Read this address to get the # of words waiting in the upload FIFO.
+    _FIFO_ADDR = 0  # Address for sending/receiving data to/from the FPGA.
+    _STATUS_ADDR = 1  # Address for getting status on the send/recv comm channels.
+    _CONTROL_ADDR = 1  # Write to this address to reset the entire comm channel.
+    _DN_FREE_ADDR = 2  # Read this address to get the amount of free space in the download FIFO.
+    _UP_USED_ADDR = 3  # Read this address to get the # of words waiting in the upload FIFO.
 
     def __init__(
         self,
         xsusb_id=DEFAULT_XSUSB_ID,
         module_id=DEFAULT_MODULE_ID,
         xsjtag=None
-        ):
+    ):
         """Setup a communication object.
-        
+
         xsusb_id = The ID for the USB port.
         module_id = The ID for the DUT I/O module in the FPGA.
         xsjtag = The Xsjtag USB port object. (Use this if not using xsusb_id.)
@@ -59,51 +62,83 @@ class XsComm:
         logging.debug('address width = '
                       + str(self._memio.address_width))
         logging.debug('data width = ' + str(self._memio.data_width))
-        
+
     def reset(self):
-        """Reset the communication hardware in the FPGA."""
+        """Reset the communication channel hardware in the FPGA."""
         self._memio.write(self._CONTROL_ADDR, [0])
-        
+
     def get_send_buffer_space(self):
         """Return the amount of space available in the FPGA to receive data."""
         return self._memio.read(self._DN_FREE_ADDR, 1).unsigned
-        
+
     def get_recv_buffer_length(self):
         """Return the amount of data waiting in the FPGA to be transmitted."""
         return self._memio.read(self._UP_USED_ADDR, 1).unsigned
-        
+
     def get_levels(self):
-        """Get the amount of space available to receive data and the amount of data
-        waiting to be transmitted."""
+        """Get the amount of space available in the FPGA to receive data and the amount of data
+        waiting in the FPGA to be transmitted."""
         print "available = %d  waiting = %d" % (self.get_send_buffer_space(), self.get_recv_buffer_length())
 
-    def send(self, packet):
-        """Send a packet of data through the comm channel to the FPGA."""
+    def send(self, buffer, wait=True):
+        """Send buffer contents through the comm channel to the FPGA.
 
-        if isinstance(packet, int):
-            packet = [packet]
-        if len(packet) == 0:
+        Keyword arguments:
+        buffer -- List of words or a single integer to send to the FPGA.
+        wait -- If true, wait until space is available in the FPGA to accept the buffer (default True).
+        """
+
+        if isinstance(buffer, int):
+            buffer = [buffer]
+        if len(buffer) == 0:
             return
 
-        if self.get_send_buffer_space() >= len(packet):
-            self._memio.write(self._FIFO_ADDR, packet)
+        space_avail = self.get_send_buffer_space()
+
+        if space_avail < len(buffer) and not wait:
+            raise XsCommException('Not enough room to send transmit buffer.')
+
+        num_words_sent = 0
+        while num_words_sent < len(buffer):
+            if space_avail is not 0:
+                self._memio.write(self._FIFO_ADDR, buffer[num_words_sent: num_words_sent + space_avail])
+                num_words_sent += space_avail
+            space_avail = self.get_send_buffer_space()
+
+    def receive(self, num_words=None, wait=True, drain=True):
+        """Return a buffer of data received from the FPGA through the comm channel.
+
+        Keyword arguments:
+        num_words -- The number of words to get from the FPGA (default None).
+        wait -- If true, wait until the number of words requested is available (default True).
+        drain -- If true and num_words==None, then take everything currently stored in the FPGA transmit buffer (default True).
+        """
+
+        num_words_avail = self.get_recv_buffer_length()
+
+        if drain and num_words is None:
+            return self._memio.read(self._FIFO_ADDR, num_words_avail)
+
+        if num_words_avail < num_words and not wait:
+            raise XsCommException('Too little data to fill receive buffer.')
+
+        buffer = []
+        num_words_needed = num_words
+        while num_words_needed > 0:
+            if num_words_avail is not 0:
+                buffer.extend(self._memio.read(self._FIFO_ADDR, min(num_words_needed, num_words_avail)))
+                num_words_needed = num_words - len(buffer)
+            num_words_avail = self.get_recv_buffer_length()
+        if num_words == 1:
+            return buffer[0]
         else:
-            raise XsCommException('Not enough room to send data packet.')
-            
-    def receive(self, num_data=0):
-        """Receive a packet of data from the FPGA through the comm channel."""
-        
-        if num_data == 0:
-            return []
-        
-        if self.get_recv_buffer_length() >= num_data:
-            return self._memio.read(self._FIFO_ADDR, num_data)
-        else:
-            raise XsCommException('Too little data to fill data packet.')
+            return buffer
 
 if __name__ == '__main__':
-    #logging.root.setLevel(logging.DEBUG)
-    
+    # logging.root.setLevel(logging.DEBUG)
+
+    print '\n', '='*70, "\nThe FPGA should be freshly loaded before running this test script!\n", '='*70, '\n'
+
     USB_ID = 0  # This is the USB index for the XuLA board connected to the host PC.
     comm = XsComm(xsusb_id=USB_ID)
     print comm._memio._get_mem_widths()
@@ -112,11 +147,11 @@ if __name__ == '__main__':
     recv = comm.receive(14)
     print "Receive = ", [d.unsigned for d in recv]
     comm.get_levels()
-    recv = comm.receive(7)
+    recv = comm.receive(drain=True)
     print "Receive = ", [d.unsigned for d in recv]
     comm.get_levels()
-    comm.send([15,16,17,18,19,20,21,22,23,24])
-    comm.send([15,16,17,18,19,20,21,22,23,24])
+    comm.send([15, 16, 17, 18, 19, 20, 21, 22, 23, 24])
+    comm.send([15, 16, 17, 18, 19, 20, 21, 22, 23, 24])
     comm.get_levels()
     recv = comm.receive(10)
     print "Receive = ", [d.unsigned for d in recv]
@@ -124,23 +159,21 @@ if __name__ == '__main__':
     recv = comm.receive(10)
     print "Receive = ", [d.unsigned for d in recv]
     comm.get_levels()
-    
+
     print "\n\nRESET\n\n"
     comm.reset()
-    
+
     comm.get_levels()
-    comm.send([1,2,3,4,5,6,7,8])
+    comm.send([1, 2, 3, 4, 5, 6, 7, 8])
     comm.get_levels()
-    comm.send([9,10,11,12,13,14,15,16])
+    comm.send([9, 10, 11, 12, 13, 14, 15, 16])
     comm.get_levels()
     recv = comm.receive(16)
     print "Receive = ", [d.unsigned for d in recv]
     comm.get_levels()
-    comm.send([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16])
+    comm.send([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
     comm.get_levels()
-    recv1 = comm.receive(8)
+    recv = comm.receive()
     comm.get_levels()
-    recv2 = comm.receive(8)
-    print "Receive = ", [d.unsigned for d in recv1+recv2]
+    print "Receive = ", [d.unsigned for d in recv]
     comm.get_levels()
-    
