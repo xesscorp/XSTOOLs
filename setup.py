e754beebd5af9f4a9a0e70cb541bc590eb2828ec
Reece Heineke <reece.heineke@gmail.com>
2016-06-09 19:53:41 -0400

    Setup changes

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
-import setuptools
-
-import xstools
-
-import sys
 import os
 import shutil
 import subprocess
+import sys
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+import setuptools
+from setuptools import setup
+
+import xstools
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -19,13 +15,16 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read().replace('.. :changelog:', '')
 
 requirements = [
-    'pypubsub >= 3.1.2',
-    'pyusb >= 1.0.0a3', 
     'bitstring >= 3.1.1', 
     'intelhex >= 1.4',
+    'pyserial >= 2.7',
+    'pypubsub >= 3.1.2',
+    'pyusb >= 1.0.0',
+    # 'wheel >= 0.23.0',
 ]
 
 test_requirements = [  # TODO: put package test requirements here
+    ''
 ]
 
 
@@ -37,10 +36,9 @@ setup(
     author=xstools.__author__,
     author_email=xstools.__email__,
     url='https://github.com/xesscorp/XSTOOLs',
-#    packages=['xstools'],
     packages=setuptools.find_packages(),
     entry_points={
-        'console_scripts':[
+        'console_scripts': [
             'usb2serial = xstools.usb2serial:usb2serial',
             'xsflags = xstools.xsflags:xsflags',
             'xsload = xstools.xsload:xsload',
@@ -51,21 +49,28 @@ setup(
             'gxstools = xstools.gxstools:gxstools',
         ],
     },
-    package_dir={'xstools': 'xstools'},
-# Don't set include_package_data to True! Then it only includes data files under version control.
-#    include_package_data=True,
+    # package_dir={'': 'xstools'},
+    # Don't set include_package_data to True! Then it only includes data files under version
+    # control.
+    #    include_package_data=True,
     package_data={'xstools': ['xula*/*.bit', 'xula*/*.hex', '*.rules', 'icons/*.png']},
     install_requires=requirements,
-    license="GPLv2+",
+    license='GPLv2+',
     zip_safe=False,
     keywords='xstools',
-    classifiers=['Development Status :: 2 - Pre-Alpha',
-                 'Intended Audience :: Developers',
-                 'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
-                 'Natural Language :: English',
-                 "Programming Language :: Python :: 2",
-                 'Programming Language :: Python :: 2.6',
-                 'Programming Language :: Python :: 2.7', ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',  # 'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Operating System :: Microsoft :: Windows :: Windows NT/2000',
+        'Operating System :: MacOS :: MacOS X',
+    ],
     test_suite='tests',
     tests_require=test_requirements,
     )
